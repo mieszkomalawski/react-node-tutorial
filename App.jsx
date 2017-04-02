930d62255c2755aaf319d8822cfb96681d28da47
Mieszko Malawski <mieszko.malawski@getresponse.com>
2017-04-02 18:16:00 +0200

    seperate components

--- App.jsx
+++ App.jsx
@@ -1,24 +1,45 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
+    render() {
 
-   var i = 1;
 
-   var myStyle = {
-      fontSize: 100,
-      color: '#FF0000'
-   };
 
-      return (
-         <div>
-            <h1 style = {myStyle}>Hello</h1>
-            <h1>Javscript example {1 == 1 ? 'True' : 'False'}</h1>
-            <p data-myattribute="value">Elo</p>
-            {//Some comment}
-         </div>
-      );
-   }
+        return (
+            <div>
+                <Header/>
+                <Content/>
+            </div>
+        );
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        var i = 1;
+
+        var myStyle = {
+            fontSize: 100,
+            color: '#FF0000'
+        };
+
+       return (
+           <div>
+               <h1 style={myStyle}>Hello</h1>
+               <h1>Javscript example {1 == 1 ? 'True' : 'False'}</h1>
+           </div>
+       )
+    }
+}
+
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <p data-myattribute="value">Elo</p>
+            </div>
+        )
+    }
 }
 
 export default App;
