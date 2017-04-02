25b5d187fbe1cbf4c9b6fffb5bc9f545134bc445
Mieszko Malawski <mieszko.malawski@getresponse.com>
2017-04-02 21:11:26 +0200

    routing not working :(

--- App.jsx
+++ App.jsx
@@ -1,51 +1,50 @@
 import React from 'react';
 
+
 class App extends React.Component {
-
-    constructor() {
-        super();
-
-        this.state = {
-            data:
-            [
-                {
-                    component: 'First...',
-                    id: 1
-                },
-
-                {
-                    component: 'Second...',
-                    id: 2
-                },
-
-                {
-                    component: 'Third...',
-                    id: 3
-                }
-            ]
-        }
-    }
-
     render() {
         return (
             <div>
-                <div>
-                    {this.state.data.map((dynamicComponent, i) => <Content key={i} componentData = {dynamicComponent} /> )}
-                </div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+
+                {this.props.children}
             </div>
-        );
+        )
     }
 }
 
-class Content extends React.Component {
+class Home extends React.Component {
     render() {
         return (
             <div>
-                <div>{this.props.componentData.component}</div>
-                <div>{this.props.componentData.id}</div>
+                <h1>Home...</h1>
             </div>
-        );
+        )
     }
 }
 
-export default App;
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>About...</h1>
+            </div>
+        )
+    }
+}
+
+class Contact extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Contact...</h1>
+            </div>
+        )
+    }
+}
+
+export {App, Home, About, Contact};
--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router';
+import { Router, Route } from 'react-router';
+import {App, Home, Contact, About} from './App.jsx';
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render((
+    <Router history = {browserHistory}>
+        <Route path = "/" component = {App}>
+            <IndexRoute component = {Home} />
+            <Route path = "home" component = {Home} />
+            <Route path = "about" component = {About} />
+            <Route path = "contact" component = {Contact} />
+        </Route>
+    </Router>
+
+), document.getElementById('app'))
