28d77a71f1ff728051d69d6144426e72405ab9b6
Mieszko Malawski <mieszko.malawski@getresponse.com>
2017-04-02 19:16:55 +0200

    state and props

--- App.jsx
+++ App.jsx
@@ -2,42 +2,20 @@ import React from 'react';
 
 class App extends React.Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         this.state = {
-            data:
-            [
-                {
-                    "id":1,
-                    "name":"Foo",
-                    "age":"20"
-                },
-
-                {
-                    "id":2,
-                    "name":"Bar",
-                    "age":"30"
-                },
-
-                {
-                    "id":3,
-                    "name":"Baz",
-                    "age":"40"
-                }
-            ]
+            header: 'Header from state'
         }
     }
 
     render() {
         return (
             <div>
-                <Header/>
-                <table>
-                    <tbody>
-                    {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
-                    </tbody>
-                </table>
+                <Header headerProp = {this.state.header} />
+                <Header2 headerProp = {this.props.headerProp} />
+                <Content contentProp = {this.props.contentProp} />
             </div>
         );
     }
@@ -45,33 +23,39 @@ class App extends React.Component {
 
 class Header extends React.Component {
     render() {
-        var i = 1;
-
-        var myStyle = {
-            fontSize: 100,
-            color: '#FF0000'
-        };
-
-        return (
+        return(
             <div>
-                <h1 style={myStyle}>Hello</h1>
-                <h1>Javscript example {1 == 1 ? 'True' : 'False'}</h1>
+                <h2>{this.props.headerProp}</h2>
             </div>
         )
     }
 }
 
-class TableRow extends React.Component {
-    render () {
-        return (
-            <tr>
-                <td>{this.props.data.id}</td>
-                <td>{this.props.data.name}</td>
-                <td>{this.props.data.age}</td>
-            </tr>
-        );
+class Header2 extends React.Component {
+    render() {
+        return(
+            <div>
+                <h3>{this.props.headerProp}</h3>
+            </div>
+        )
     }
 }
+
+class Content extends React.Component {
+    render() {
+        return(
+            <div>
+                <p>{this.props.contentProp}</p>
+            </div>
+        )
+    }
+}
+
+App.defaultProps = {
+    headerProp: "Header from props default...",
+    contentProp: "dupa"
+}
+
 
 
 export default App;
