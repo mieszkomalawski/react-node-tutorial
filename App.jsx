82c1cbcd335b77098d8da4547b7f10fb1d5d9e0a
Mieszko Malawski <mieszko.malawski@getresponse.com>
2017-04-02 18:02:40 +0200

    simple example

--- App.jsx
+++ App.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 class App extends React.Component {
    render() {
+
+   var i = 1;
+
+   var myStyle = {
+      fontSize: 100,
+      color: '#FF0000'
+   };
+
       return (
          <div>
-            Hello World!!!
+            <h1 style = {myStyle}>Hello</h1>
+            <h1>Javscript example {1 == 1 ? 'True' : 'False'}</h1>
+            <p data-myattribute="value">Elo</p>
+            {//Some comment}
          </div>
       );
    }
