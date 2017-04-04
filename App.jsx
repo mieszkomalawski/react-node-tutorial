1faa30346dc99b0963a9a4998d982bcbdd237354
Mieszko Malawski <mieszko.malawski@getresponse.com>
2017-04-04 20:17:50 +0200

    fade int

--- App.jsx
+++ App.jsx
@@ -1,50 +1,20 @@
 import React from 'react';
-
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 class App extends React.Component {
+
     render() {
         return (
             <div>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
+                <ReactCSSTransitionGroup transitionName = "example"
+                                         transitionAppear = {true} transitionAppearTimeout = {500}
+                                         transitionEnter = {false} transitionLeave = {false}>
 
-                {this.props.children}
+                    <h1>My Element...</h1>
+                </ReactCSSTransitionGroup>
             </div>
-        )
+        );
     }
 }
 
-class Home extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Home...</h1>
-            </div>
-        )
-    }
-}
-
-class About extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>About...</h1>
-            </div>
-        )
-    }
-}
-
-class Contact extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Contact...</h1>
-            </div>
-        )
-    }
-}
-
-export {App, Home, About, Contact};
+export default App;
--- main.js
+++ main.js
@@ -1,17 +1,5 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
-import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router';
-import { Router, Route } from 'react-router';
-import {App, Home, Contact, About} from './App.jsx';
+import App from './App.jsx';
 
-ReactDOM.render((
-    <Router history = {browserHistory}>
-        <Route path = "/" component = {App}>
-            <IndexRoute component = {Home} />
-            <Route path = "home" component = {Home} />
-            <Route path = "about" component = {About} />
-            <Route path = "contact" component = {Contact} />
-        </Route>
-    </Router>
-
-), document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
